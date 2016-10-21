bb1e2096df6f18ed78ccb37a4105a428f70b0b18
Jernej Lipovec <neykomir@gmail.com>
2016-10-21 07:10:58 +0200

    Added saga, removed some controls

--- app/components/Main/VideoPlayerContainer.jsx
+++ app/components/Main/VideoPlayerContainer.jsx
@@ -1,12 +1,10 @@
 var VideoPlayer = require('./VideoPlayer');
-var VideoControls = require('./VideoControls');
 
 let VideoPlayerContainer = (props) => {
     return (
         <div className="col-lg-7 col-md-6 col-sm-12 no-gutter">
             <div className="main_window">
                 <VideoPlayer videos={props.playlist.videos} />
-                <VideoControls />
             </div>
         </div>
     );
